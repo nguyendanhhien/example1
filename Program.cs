6638e5559917fa2f48f8085af445f34f46e89b43
nguyendanhhien <nguyendanhhien2005@gmail.com>
2025-07-04 13:14:20 +0700

    add
    add

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
-﻿public class program
+﻿//nhap mot so tu ban phim va hien ra man hinh
+public class Program
 {
-    public static void main(String[] args)
+    public static void Main(String[] args)
     {
-
+        Console.WriteLine("hay nhap mot so nguyen bat ki");
+        int so = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("" + so);
     }
 }
